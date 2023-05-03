ad2265cf10b869034f5d657b6b1bc621afba389b
Iveth Plata <karen.platahdz22@outlook.es>
2023-05-03 00:24:24 -0600

    Mensajes de conexion y desconexion

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,20 @@
+// Referencias del HTML
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 const socket = io();
+
+// Escuchar eventos
+socket.on('connect', () => {
+    console.log('Conectado');
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+    console.log('Desconectado del servidor');
+
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+});
