53beb6ed027485cc9355829c510b50a11871acf0
Iveth Plata <karen.platahdz22@outlook.es>
2023-05-03 00:08:53 -0600

    instalar socket.io

--- models/server.js
+++ models/server.js
@@ -4,8 +4,10 @@ const cors = require('cors');
 class Server {
 
     constructor() {
-        this.app  = express();
-        this.port = process.env.PORT;
+        this.app        = express();
+        this.port       = process.env.PORT;
+        this.server     = require('http').createServer( this.app );
+        this.io         = require('socket.io')( this.server );
 
         this.paths = {
             
@@ -32,7 +34,7 @@ class Server {
     }
 
     listen() {
-        this.app.listen( this.port, () => {
+        this.server.listen( this.port, () => {
             console.log('Servidor corriendo en puerto', this.port );
         });
     }
